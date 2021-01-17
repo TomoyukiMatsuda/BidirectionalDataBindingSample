f303194d92765b551831bb536dd92b3b90aefb02
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-01-17 12:58:13 +0900

    フォーム入力テキストをリアルタイムで表示させる

--- app/src/main/java/com/android/bidirectionaldatabindingsample/MainActivity.java
+++ app/src/main/java/com/android/bidirectionaldatabindingsample/MainActivity.java
@@ -1,20 +1,19 @@
 package com.android.bidirectionaldatabindingsample;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.library.baseAdapters.BR;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 
 import com.android.bidirectionaldatabindingsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String formText;
-    private String displayText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/android/bidirectionaldatabindingsample/ViewModel.java
+++ app/src/main/java/com/android/bidirectionaldatabindingsample/ViewModel.java
@@ -1,26 +1,33 @@
 package com.android.bidirectionaldatabindingsample;
 
 import android.text.TextUtils;
-
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
 import androidx.databinding.library.baseAdapters.BR;
 
 public class ViewModel extends BaseObservable {
     private String formText;
-    private String displayText;
 
     @Bindable public String getFormText() {
-        return this.formText;
+        return formText;
     }
 
-    @Bindable public void setFormText(String formText) {
+    // setには @Bindable　不要そう
+    public void setFormText(String formText) {
         this.formText = formText;
+        notifyPropertyChanged(BR.displayText);
         notifyPropertyChanged(BR.buttonEnable);
+    }
+
+    @Bindable
+    public String getDisplayText() {
+        // フォーム入力のテキストを代入
+        String displayText = formText;
+        return displayText;
     }
 
     // EditTextに入力があるかどうかでボタン活性・非活性を制御
     @Bindable public boolean isButtonEnable() {
-        return !TextUtils.isEmpty(this.formText);
+        return !TextUtils.isEmpty(formText);
     }
 }
