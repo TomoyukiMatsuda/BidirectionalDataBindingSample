3d6f2ab26622d142a889c8f0c05ddb00ffa7eac7
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-01-17 13:49:14 +0900

    ボタンクリックでテキストの表示＋フォームの初期化

--- app/src/main/java/com/android/bidirectionaldatabindingsample/ViewModel.java
+++ app/src/main/java/com/android/bidirectionaldatabindingsample/ViewModel.java
@@ -8,6 +8,7 @@ import androidx.databinding.library.baseAdapters.BR;
 public class ViewModel extends BaseObservable {
     private String formText;
 
+    // ゲット
     @Bindable public String getFormText() {
         return formText;
     }
@@ -15,19 +16,31 @@ public class ViewModel extends BaseObservable {
     // setには @Bindable　不要そう
     public void setFormText(String formText) {
         this.formText = formText;
-        notifyPropertyChanged(BR.displayText);
+        notifyPropertyChanged(BR.realTimeText);
         notifyPropertyChanged(BR.buttonEnable);
     }
 
+    // メソッド名とは違う処理も合わせて行っているのがあまりよくなさそう
+    @Bindable public String getClickText() {
+        String clickText = formText;
+        formText = "";
+        notifyPropertyChanged(BR.formText);
+        return clickText;
+    }
+
     @Bindable
-    public String getDisplayText() {
+    public String getRealTimeText() {
         // フォーム入力のテキストを代入
-        String displayText = formText;
-        return displayText;
+        String realTimeText = formText;
+        return realTimeText;
     }
 
     // EditTextに入力があるかどうかでボタン活性・非活性を制御
     @Bindable public boolean isButtonEnable() {
         return !TextUtils.isEmpty(formText);
+    }
+
+    public void onButtonClick() {
+        notifyPropertyChanged(BR.clickText);
     }
 }
